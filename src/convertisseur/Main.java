10efdf330c6f8e80ca113ae6631a245d6e763a0b
Benoit Bertrand <benoit.bertrand@live.fr>
2023-03-11 16:08:49 +0100

    javadoc main

--- src/convertisseur/Main.java
+++ src/convertisseur/Main.java
@@ -1,5 +1,14 @@
 package convertisseur;
 
+/**
+ * Main class of the application.
+ * "Convertisseur de devises" is a simple application that converts a given amount of money from one currency to another.
+ * TP_02 : Programmation Fonctionnelle en Java
+ * PEI Millau, CAI-SI 03 : (2022-2023)
+ * CNAM, NFP101 : Programmation Fonctionnelle en Java
+ * @author BBE
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
