d191cba1bf31c3c5be9b73c486c04ade6b0195c2
Benoit Bertrand <benoit.bertrand@live.fr>
2023-03-12 16:05:08 +0100

    Ajout d'un test sur ConvertisseurTest

--- src/testsConvertisseur/testsPoo/ConvertisseurTest.java
+++ src/testsConvertisseur/testsPoo/ConvertisseurTest.java
@@ -14,23 +14,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @version 1.0
  */
 public class ConvertisseurTest {
+
+    final Devise DEVISE_DOLLAR = new Devise("Dollar", "$", 0.94F);
+    final Devise DEVISE_EURO = new Devise("Euro", "€", 1.00F);
     /*
      * Plan de test de la méthode convertir.
      * 1. Test de la conversion d'un montant en euro vers un montant en dollar.
      * TODO : Ajouter d'autres tests.
      */
 
-
     /**
      * Test de la méthode convertir.
      */
     @Test
     public void testConvertir() {
-        System.out.println("testConvertir");
-        Montant montant = new Montant(1.10F, new Devise("Euro", "€", 1.00F));
-        Devise devise = new Devise("Dollar", "$", 0.94F);
-        Montant expResult = new Montant(1.17F, new Devise("Dollar", "$", 0.94F));
-        Montant result = Convertisseur.convertir(montant, devise);
+        System.out.println("testConvertir : est OK");
+        Montant montant = new Montant(1.10F, DEVISE_EURO);
+        Montant expResult = new Montant(1.17F, DEVISE_DOLLAR);
+        Montant result = Convertisseur.convertir(montant, DEVISE_DOLLAR);
         assertEquals(expResult, result);
     }
 
@@ -39,7 +40,7 @@ public class ConvertisseurTest {
      */
     @Test
     public void testArrondirMontant() {
-        System.out.println("testArrondirMontant");
+        System.out.println("testArrondirMontant : est OK");
         float montant = 1.123456F;
         float expResult = 1.12F;
         float result = Convertisseur.arrondirMontant(montant);
@@ -51,9 +52,9 @@ public class ConvertisseurTest {
      */
     @Test
     public void testConvertirVersEuro() {
-        System.out.println("testConvertirVersEuro");
-        Montant montant = new Montant(1.00F, new Devise("Dollar", "$", 0.94F));
-        Montant expResult = new Montant(0.94F, new Devise("Euro", "€", 1.00F));
+        System.out.println("testConvertirVersEuro : est OK");
+        Montant montant = new Montant(1.00F, DEVISE_DOLLAR);
+        Montant expResult = new Montant(0.94F, DEVISE_EURO);
         Montant result = Convertisseur.convertirVersEuro().apply(montant);
         assertEquals(expResult, result);
     }
@@ -63,10 +64,10 @@ public class ConvertisseurTest {
      */
     @Test
     public void testConvertirVersDevise() {
-        System.out.println("testConvertirVersDevise");
-        Montant montant = new Montant(1.00F, new Devise("Euro", "$", 1.00F));
-        Montant expResult = new Montant(1.06F, new Devise("Dollar", "$", 0.94F));
-        Montant result = Convertisseur.convertirVersDevise(new Devise("Dollar", "$", 0.94F)).apply(montant);
+        System.out.println("testConvertirVersDevise : est OK");
+        Montant montant = new Montant(1.00F, DEVISE_EURO);
+        Montant expResult = new Montant(1.06F, DEVISE_DOLLAR);
+        Montant result = Convertisseur.convertirVersDevise(DEVISE_DOLLAR).apply(montant);
         assertEquals(expResult, result);
     }
 
@@ -75,10 +76,10 @@ public class ConvertisseurTest {
      */
     @Test
     public void testConvertirVersDevise2() {
-        System.out.println("testConvertirVersDevise2");
-        Montant montant = new Montant(1.00F, new Devise("Euro", "$", 1.00F));
-        Montant expResult = new Montant(1.06F, new Devise("Dollar", "$", 0.94F));
-        Montant result = Convertisseur.convertirVersDevise(new Devise("Dollar", "$", 0.94F)).apply(montant);
+        System.out.println("testConvertirVersDevise2 : est OK");
+        Montant montant = new Montant(1.00F, DEVISE_EURO);
+        Montant expResult = new Montant(1.06F, DEVISE_DOLLAR);
+        Montant result = Convertisseur.convertirVersDevise(DEVISE_DOLLAR).apply(montant);
         assertEquals(expResult, result);
     }
 }
